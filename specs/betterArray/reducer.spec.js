1b204eeedb9fa40af33662ade20d28c178c134ce
Ramon Rodrigues <cerberus.ramon@gmail.com>
2019-05-04 17:20:29 +0100

    BA04 Added specs for most operations

--- specs/betterArray/reducer.spec.js
+++ specs/betterArray/reducer.spec.js
@@ -1,59 +1,63 @@
 import BetterArray from '../../src/betterArray';
 
-describe('reducer based operations', () => {
+describe('reducer operations', () => {
   let betterArray;
 
   beforeEach(() => {
     betterArray = new BetterArray()
   })
 
-  test('sum should work with an empty array' , () => {
-    expect(betterArray.sum()).toBe(0)
+  describe('from empty arrays', () => {
+    test('sum should return 0' , () => {
+      expect(betterArray.sum()).toBe(0)
+    })
+
+    test('subtraction should raise an error', () => {
+      expect(() => { betterArray.subtraction() }).toThrowError(TypeError)
+    })
+
+    test('product should raise an error', () => {
+      expect(() => { betterArray.product() }).toThrowError(TypeError)
+    })
+
+    test('maximum should raise an error', () => {
+      expect(() => { betterArray.maximum() }).toThrowError(TypeError)
+    })
+
+    test('minimum should raise an error', () => {
+      expect(() => { betterArray.minimum() }).toThrowError(TypeError)
+    })
   })
 
-  test('sum should be the result of adding all the values', () => {
-    const sumValue = 1 + 2 + 3;
-    betterArray.from([1,2,3])
-    expect(betterArray.sum()).toBe(sumValue)
-  })
+  describe('non empty arrays', () => {
+    test('sum should be the result of adding all the values', () => {
+      const sumValue = 1 + 2 + 3;
+      betterArray.from([1, 2, 3])
+      expect(betterArray.sum()).toBe(sumValue)
+    })
 
-  test('subtraction should work with an empty array', () => {
-    expect(betterArray.subtraction()).toBe(0)
-  })
+    test('subtraction should be the result of subtracting all the values', () => {
+      const subtractionValue = 1 - 2 - 10;
+      betterArray.from([1, 2, 10])
+      expect(betterArray.subtraction()).toBe(subtractionValue)
+    })
 
-  test('subtraction should be the result of subtracting all the values', () => {
-    const subtractionValue = 1 - 2 - 10;
-    betterArray.from([1, 2, 10])
-    expect(betterArray.subtraction()).toBe(subtractionValue)
-  })
+    test('product should be the result of multiplying all the values', () => {
+      const productValue = 1 * 2 * 3;
+      betterArray.from([1, 2, 3])
+      expect(betterArray.product()).toBe(productValue)
+    })
 
-  test('product should work with an empty array', () => {
-    expect(betterArray.product()).toBe(1)
-  })
+    test('maximum returns the highest value', () => {
+      const maximumValue = 7;
+      betterArray.from([7, 1, 2, 3])
+      expect(betterArray.maximum()).toBe(maximumValue)
+    })
 
-  test('product should be the result of subtracting all the values', () => {
-    const productValue = 1 * 2 * 3;
-    betterArray.from([1, 2, 3])
-    expect(betterArray.product()).toBe(productValue)
-  })
-
-  test('maximum should not work', () => {
-    expect(() => {betterArray.maximum()}).toThrowError(TypeError)
-  })
-
-  test('maximum should be the result of subtracting all the values', () => {
-    const maximumValue = 7;
-    betterArray.from([7, 1, 2, 3])
-    expect(betterArray.maximum()).toBe(maximumValue)
-  })
-
-  test('minimum should not work', () => {
-    expect(() => { betterArray.minimum() }).toThrowError(TypeError)
-  })
-
-  test('minimum should be the result of subtracting all the values', () => {
-    const minimumValue = 1;
-    betterArray.from([7, 1, 2, 3])
-    expect(betterArray.minimum()).toBe(minimumValue)
+    test('minimum returns lowest value', () => {
+      const minimumValue = 1;
+      betterArray.from([7, 1, 2, 3])
+      expect(betterArray.minimum()).toBe(minimumValue)
+    })
   })
 })
